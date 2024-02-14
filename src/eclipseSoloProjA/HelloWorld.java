0600975f372f8ab1a4f5ce14d8e7569d99374352
Minglun <mwei@emich.edu>
2024-02-13 22:31:32 -0500

    second commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,5 +3,6 @@ package eclipseSoloProjA;
 public class HelloWorld {
   public static void main(String[] args) {
 	  System.out.println("First statement");
+	  System.out.println("Second statement");
   }
 }
